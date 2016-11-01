1dfc1cf4a10887011a0e0a700f15e9f9d9219162
Guillaume de Chambrier <chambrierg@gmail.com>
2016-11-01 13:16:15 +0100

    restructed

--- python/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('demo',
-                    sources = ['demo.c'])
-
-setup(name = 'npr',
-      version = '1.0',
-      description = 'This is a demo package',
-      ext_modules = ['pylwr.so'])
--- test/lwr_test.cpp
+++ test/lwr_test.cpp
@@ -2,51 +2,48 @@
 #include <boost/chrono.hpp>
 #include <boost/lexical_cast.hpp>
 
-double evaluate_lwr(int dim,int num_data,int num_test){
+TEST(LWRTest,1DRegression)
+{
+
+  lwr::lwr_options lwr_opts;
+
+  lwr_opts.D.resize(dim);
+  for(int i = 0; i < dim;i++){
+      lwr_opts.D[i] = 1*1;
+  }
+
+  lwr_opts.k_bias = 1;
+  lwr_opts.y_bias = 0;
+  lwr_opts.bUseKDT = true;
+  lwr::LWR Lwr(lwr_opts);
+  Lwr.mtype = conversion::ROW_M;
+
+  // Generate data
+  arma::mat    X(num_data,dim,arma::fill::randu);
+
+  X = X.st();
 
 
+  X = X * 10;
+  arma::colvec y(num_data,arma::fill::randu);
+  arma::mat Xtest(num_test,dim,arma::fill::randu);
+  Xtest = Xtest.st();
+  double *ptr_y = new double[num_test];
 
-    lwr::lwr_options lwr_opts;
+  Lwr.set_X(X);
+  Lwr.set_Y(y);
 
-    lwr_opts.D.resize(dim);
-    for(int i = 0; i < dim;i++){
-        lwr_opts.D[i] = 1*1;
-    }
+  auto start = boost::chrono::steady_clock::now();
 
-    lwr_opts.k_bias = 1;
-    lwr_opts.y_bias = 0;
-    lwr_opts.bUseKDT = true;
+  Lwr.f(ptr_y,Xtest);
 
-    lwr::LWR Lwr(lwr_opts);
-    Lwr.mtype = conversion::ROW_M;
+  auto end = boost::chrono::steady_clock::now();
 
-    // Generate data
-    arma::mat    X(num_data,dim,arma::fill::randu);
+  auto diff = end - start;
 
-    X = X.st();
-
-
-    X = X * 10;
-    arma::colvec y(num_data,arma::fill::randu);
-    arma::mat Xtest(num_test,dim,arma::fill::randu);
-    Xtest = Xtest.st();
-    double *ptr_y = new double[num_test];
-
-    Lwr.set_X(X);
-    Lwr.set_Y(y);
-
-    auto start = boost::chrono::steady_clock::now();
-
-    Lwr.f(ptr_y,Xtest);
-
-    auto end = boost::chrono::steady_clock::now();
-
-    auto diff = end - start;
-
-    return boost::chrono::duration <double, boost::milli> (diff).count() / 1000.0;
+  return boost::chrono::duration <double, boost::milli> (diff).count() / 1000.0;
 
 }
-
 
 
 int main(int argc,char** argv){
