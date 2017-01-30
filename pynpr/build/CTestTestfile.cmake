64a0218e6f29e9a48a9482d92d90f5b4475010aa
Guillaume de Chambrier <chambrierg@gmail.com>
2017-01-30 14:19:57 +0100

    removed build

--- pynpr/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guillaume/cpp_work_space/non-parametric-regression/pynpr
-# Build directory: /home/guillaume/cpp_work_space/non-parametric-regression/pynpr/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
