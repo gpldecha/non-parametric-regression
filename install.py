2ccc92f6cd3b543e505133e19945bb61247d7bf8
Guillaume de Chambrier <chambrierg@gmail.com>
2017-01-31 10:54:18 +0100

    added install file

---
+++ install.py
@@ -0,0 +1,52 @@
+import apt
+import sys
+import os
+import shutil
+import subprocess
+
+if __name__ == "__main__":
+
+
+    print '=== Installing ==='
+
+    dependencies = ['build-essential', 'libarmadillo-dev','libboost-python-dev',
+                    'libflann-dev','python-numpy']
+
+    cache = apt.Cache()
+
+    W  = '\033[0m'  # white (normal)
+    R  = '\033[31m' # red
+    G  = '\033[32m' # green
+
+    for package in dependencies:
+
+        pkg = cache[package]
+        if pkg.is_installed:
+            print package, '    ', G+'found'+W
+        else:
+            print package, '    ', R+'missing'+W
+            pkg.mark_install()
+            try:
+                cache.commit()
+            except Exception, arg:
+                print >> sys.stderr, "Sorry, package installation failed [{err}]".format(err=str(arg))
+
+    # build and install NPR package
+    if os.path.exists('./npr/build'):
+        shutil.rmtree('./npr/build')
+    os.makedirs('./npr/build')
+    os.chdir( './npr/build' )
+    os.system('cmake ..')
+    os.system('make')
+    os.system('sudo make install')
+    os.chdir('../..')
+
+    # build and install PYNPR
+    if os.path.exists('./pynpr/build'):
+        shutil.rmtree('./pynpr/build')
+    os.makedirs('./pynpr/build')
+    os.chdir( './pynpr/build' )
+    os.system('cmake ..')
+    os.system('make')
+    os.system('sudo make install')
+    os.chdir('../..')
